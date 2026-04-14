62e6b599bd3e73a8afc18041f129ed2b5ebc0d77
iqra027 <iqradahir72@gmail.com>
2026-04-14 07:49:16 +0300

    Exercise 10 js

---
+++ Exercise10/arrayOfObejects.js
@@ -0,0 +1,15 @@
+let book=[
+    {
+    title:"Atomic Habits",
+    author:"James Clear",
+    year:2018
+    },
+    {
+    title:"The Power of Habit",
+    author:"Charles Duhigg",
+    year:2012
+    }
+]
+
+console.log(book[0].title);
+console.log(book[1].author);
