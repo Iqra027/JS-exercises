dddc6d45fac72ec68c407c29f58cd9be76a6cc2f
iqra027 <iqradahir72@gmail.com>
2026-04-14 07:12:49 +0300

    Updated exercise 9 in ex8

--- Exercise8/object.js
+++ Exercise8/object.js
@@ -3,7 +3,12 @@ let car={
     make:"Toyota",
     model:"Camry",
     year:2020,
+    start:function(){
+        console.log("The car has started");
+    }
 }
+
 console.log(car.make);
 console.log(car.model);
 console.log(car.year);
+console.log(car.start());
