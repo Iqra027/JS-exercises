b50c8d1c30e9ee4fa0fbdc3147596bfba9ffd0a5
iqra027 <iqradahir72@gmail.com>
2026-04-14 20:34:50 +0300

    Complete Ex23 in Ex22 folder

--- Exercise22/arrayDestructuring.js
+++ Exercise22/arrayDestructuring.js
@@ -1,4 +1,16 @@
-const colors=["red","green","blue"]
-const [Red,,Blue]=colors
-console.log(Red)
-console.log(Blue)
+// const colors=["red","green","blue"]
+// const [Red,,Blue]=colors
+// console.log(Red)
+// console.log(Blue)
+
+
+// Object destruction
+
+let car={
+    make:"Toyota",
+    model:"carolla",
+    year:2000
+ }
+ let {make,model}=car
+ console.log(make)
+ console.log(model)
