df74101a4098086361c0dc19c9978ed6f0170d86
iqra027 <iqradahir72@gmail.com>
2026-04-14 09:43:34 +0300

    commited exercise 12 in exercise 11 folder

--- Exercise11/loops.js
+++ Exercise11/loops.js
@@ -1,3 +1,9 @@
-for(let i=1; i<=50; i++){
-    console.log(i);
+// for(let i=1; i<=50; i++){
+//     console.log(i);
+// }
+
+//Exercise 12
+let fruits=["apple","banana","orange","grape","kiwi","mango","strawberry"];
+for(let i=0; i<fruits.length; i++){
+    console.log(fruits[i]);
 }
