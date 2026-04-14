beb5e73115c24a3cd2db750236689c14fc371a1b
iqra027 <iqradahir72@gmail.com>
2026-04-14 10:13:37 +0300

    exercise 14 in Ex13 folder

--- Exercise13/whileLoop.js
+++ Exercise13/whileLoop.js
@@ -1,6 +1,13 @@
 
-let i=5;
-while(i>=1){
-    console.log(i);
-    i--;
-}
+// let i=5;
+// while(i>=1){
+//     console.log(i);
+//     i--;
+// }
+
+//exercise 14
+let number;
+do{
+    number=prompt("Enter a number greater than 10");
+} while(number < 10);
+console.log(`You entered a ${number} which is greater than 10.`);
