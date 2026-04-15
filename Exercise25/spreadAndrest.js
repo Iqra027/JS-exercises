4c99b652e25dba236d90e5db61ef522a33e5d5d8
iqra027 <iqradahir72@gmail.com>
2026-04-15 06:16:26 +0300

    commited Ex25

---
+++ Exercise25/spreadAndrest.js
@@ -0,0 +1,12 @@
+//spread
+const numbers=[1,2,3,4,5]
+const updatedNumbers=[...numbers,6,7,8,9]
+console.log(updatedNumbers)
+
+
+//Rest
+// function multiply(...num){
+//     return num.reduce((total,num)=>total *num ,1)
+// }
+
+// console.log(multiply(1,2,3,4,5))
