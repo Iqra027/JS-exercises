87a2ae1310bb3f6b743fb134af14eb3180711bac
iqra027 <iqradahir72@gmail.com>
2026-04-16 22:57:37 +0300

    Ex32 in js

---
+++ Exercise32/Dom.js
@@ -0,0 +1,22 @@
+
+const id=document.getElementById("title")
+console.log(id)
+const content=document.getElementsByClassName("content")
+console.log(content)
+const span=document.getElementsByTagName("span")
+console.log(span)
+const button=document.getElementsByTagName("button")
+console.log(button)
+
+
+const titles=document.querySelectorAll("h2")
+console.log(titles)
+
+const contents=document.querySelector(".content")
+console.log(contents)
+
+const spans=document.querySelector("span")
+console.log(spans)
+
+const buttons=document.querySelector("button")
+console.log(buttons)
