550b1f9576f390cdb43c107b0b9c0f53ce42e4a0
iqra027 <iqradahir72@gmail.com>
2026-04-17 11:13:39 +0300

    Ex33 in Ex32

--- Exercise32/Dom.js
+++ Exercise32/Dom.js
@@ -1,22 +1,30 @@
 
-const id=document.getElementById("title")
-console.log(id)
-const content=document.getElementsByClassName("content")
-console.log(content)
-const span=document.getElementsByTagName("span")
-console.log(span)
-const button=document.getElementsByTagName("button")
-console.log(button)
+// const id=document.getElementById("title")
+// console.log(id)
+// const content=document.getElementsByClassName("content")
+// console.log(content)
+// const span=document.getElementsByTagName("span")
+// console.log(span)
+// const button=document.getElementsByTagName("button")
+// console.log(button)
 
 
-const titles=document.querySelectorAll("h2")
-console.log(titles)
+// const titles=document.querySelectorAll("h2")
+// console.log(titles)
 
-const contents=document.querySelector(".content")
-console.log(contents)
+// const contents=document.querySelectorAll(".content")
+// console.log(contents)
 
-const spans=document.querySelector("span")
-console.log(spans)
+// const spans=document.querySelector("span")
+// console.log(spans)
 
-const buttons=document.querySelector("button")
-console.log(buttons)
+// const buttons=document.querySelector("button")
+// console.log(buttons)
+
+//Exercise 33
+
+function save() {
+    const saveButton=document.querySelector(".saveButton")
+    saveButton.textContent="Saved already"
+    saveButton.style.backgroundColor="green"
+}
